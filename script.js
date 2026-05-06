c820cb161e95e72b1deb586a594d1ee1c1162569
noah6662 <28rauschn2010@gmail.com>
2026-05-06 17:43:54 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if(entry.isIntersecting){
-      entry.target.classList.add("show");
-    }
-  });
-});
-
-document.querySelectorAll(".reveal").forEach(el => observer.observe(el));
